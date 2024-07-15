9079e783ddb4d86fb9655573594801611b5bc1b7
shiva <smbp@shiva.local>
2024-07-15 17:53:22 -0400

    .

--- pilot/pilot/main.c
+++ pilot/pilot/main.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SIZE 50
 
 struct fat_boy {
-	char v[SIZE];
+  char v[SIZE];
 };
 
 typedef struct fat_boy fat_boy;
 
-
 void find_42(fat_boy boy) {
-	int i = 0;
+  int i = 0;
 
-	while (1) {
-		if (boy.v[i] == 42) {
-			printf("42 is in position: %d\n", i);
-			break;
-		}
-		//i++;
-		++i;
-	}
+  while (1) {
+    if (boy.v[i] == 42) {
+      printf("42 is in position: %d\n", i);
+      break;
+    }
+    // i++;
+    ++i;
+  }
 }
 
-int main() {
-	fat_boy boy;
+int main(void) {
+  fat_boy boy;
 
-	srand(time(NULL));	// wtf does this do?
+  srand(time(NULL)); // wtf does this do?
 
-	boy.v[rand() % SIZE] = 42;
+  boy.v[rand() % SIZE] = 42;
 
-	find_42(boy);
+  find_42(boy);
+
+  return 0;
 }
-
