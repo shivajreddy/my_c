2d413350a3a63978b17a20cfd454668ecfaaf689
shiva <smbp@shiva.local>
2024-07-16 18:40:46 -0400

    added solutions

---
+++ chapter_4/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+  // Exercises
+  /* 4.1
+    (a) i = 5; j = 3;
+    printf("%d %d", i / j, i % j);
+
+    (b) i = 2; j = 3;
+    printf("%d", (i + 10) % j);
+
+    (c) i = 7; j = 8; k = 9;
+    printf("%d", (i + 10) % k / j);
+    int i = 7, j = 8, k = 9;
+    printf("%d\n", (17) % 9 / 8); // 1
+    printf("%d\n", (17) % 9);     // 8
+    printf("%d\n", 8 / 8);        // 1
+
+    (d) i = 1; j = 2; k = 3;
+    printf("%d", (i + 5) % (j + 2) / k);
+  */
+  printf("%d\n", 8 / 5);
+  printf("%d\n", -8 / 5);
+  printf("%d\n", 8 / -5);
+  printf("%d\n", -8 / -5);
+}
