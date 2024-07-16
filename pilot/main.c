b9cefad6dc1e82ca8c401218727e5e252afa8807
shiva <smbp@shiva.local>
2024-07-15 21:16:10 -0400

    pointer arithmetic

--- pilot/main.c
+++ pilot/main.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define SIZE 50
-
-struct fat_boy {
-  char v[SIZE];
-};
-
-typedef struct fat_boy fat_boy;
-
-void find_42(fat_boy boy) {
-  int i = 0;
-
-  while (1) {
-    if (boy.v[i] == 42) {
-      printf("42 is in position: %d\n", i);
-      break;
-    }
-    // i++;
-    ++i;
-  }
-}
 
 int main(void) {
-  fat_boy boy;
 
-  srand(time(NULL)); // wtf does this do?
+  /*
+  - Array
+  creating an array will set the value of the variable to the
+  pointer of the first element with type of the elmenet that
+  the array holds.
 
-  boy.v[rand() % SIZE] = 42;
+  int arr[6] = {16, 17, 18, 19, 20, 21};
+  printf("arr: %p\n", arr);
+  printf("arr.size %d\n", sizeof(arr));
+  printf("*arr %d\n", *arr);
+  printf("*arr+1 %d\n", *(arr + 1));
 
-  find_42(boy);
 
-  return 0;
+  - Pointer Arithmetic
+
+  */
+  /* Pointer arithmetic
+  int x = 10;
+
+  int *ptr_x = &x;
+  printf("ptr_x: %p\n", ptr_x);
+  printf("*ptr_x: %d\n", *ptr_x);
+
+  ptr_x += 1; // will point to garbage value
+  printf("ptr_x: %p\n", ptr_x);
+  printf("*ptr_x: %d\n", *ptr_x);
+  */
+
+  /*
+  char letter = 'a';
+  printf("letter: %c\n", letter);
+  printf("size of letter: %d bytes\n", sizeof(letter));
+
+  char *ptr_letter = &letter;
+  printf("&letter: %p\n", &letter);
+  printf("ptr_x: %p\n", ptr_letter);
+  */
 }
