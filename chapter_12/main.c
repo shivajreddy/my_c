8d2b5cff0fd8248544820d2423687c8d5d8ed945
shiva <shivajreddy@outlook.com>
2024-07-18 17:04:06 -0400

    .

---
+++ chapter_12/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void print_array(int *arr, int n);
+
+int main() {
+
+  /*
+  srand(time(NULL));
+
+  int arr[10] = {};
+
+  int *ptr = &arr[10 / 2];
+
+  while (ptr++ < &arr[10]) {
+    *ptr = rand() % 10;
+  }
+
+  // print array
+  for (int idx = 0; idx < 10; idx++) {
+    printf("%d ", arr[idx]);
+  }
+
+  printf("\n");
+
+  int *p, *q;
+  p = arr;
+  q = arr - 2;
+
+  int result = p >= q;
+
+  printf("result: %d", result);
+  */
+
+  // *++p
+  // *(++p)
+
+  int arr[3] = {8, 97, 62};
+  print_array(arr, 3);
+
+  int *p = arr;
+
+  printf("p=%p\n", p);
+  printf("*p=%d\n", *p);
+
+  // *p++ is same as  returning *p; then p = p+1;
+  // printf("*p++%d\n", *p++);
+  // printf("*p=%d\n", *p);
+
+  // *p++ += 1;
+  // *p++ += 1;
+  printf("p=%p\n", p);
+  printf("*p=%d\n", *p);
+
+  print_array(arr, 3);
+}
+
+void print_array(int *arr, int n) {
+  for (int idx = 0; idx < n; idx++) {
+    printf("%d ", arr[idx]);
+  }
+  printf("\n");
+}
