1b1f8830762d753f3a12a104a27c985679289746
shiva <shivajreddy@outlook.com>
2024-07-18 13:58:56 -0400

    .

--- chapter_11/main.c
+++ chapter_11/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
+// int i;
+void avg_sum(double a[], int n, double *avg, double *sum);
+
 int main() {
-  printf("hi");
-  printf("hi");
+  /*
+  double arr[] = {3, 5, 2, 10, 30};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  double total_avg, total_sum;
+  double *avg, *sum;
+  avg = &total_avg;
+  sum = &total_sum;
+
+  printf("total_avg: %f\n", total_avg);
+  printf("total_sum: %f\n", total_sum);
+
+  avg_sum(arr, n, avg, sum);
+
+  printf("total_avg: %f\n", total_avg);
+  printf("total_sum: %f\n", total_sum);
+  */
+
+  /*
+  int i = 10;
+  int *p;
+  int *q;
+  */
+
+  /*
+  a: CE
+  b: UB
+  c: CE
+  d: CE  assigning &&int to &int
+  e: p = *&q
+  f: p = q
+  g: CE
+  h: CE
+  i: *p = *q
+  */
+}
+
+void avg_sum(double a[], int n, double *avg, double *sum) {
+  int idx;
+  for (idx = 0; idx < n; idx++) {
+    *sum += a[idx];
+  }
+  *avg = *sum / idx;
 }
