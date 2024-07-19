9882a9a037289db27c5110356edb0e52e1388e7f
shiva <smbp@shiva.local>
2024-07-19 10:14:39 -0400

    .

--- chapter_13/main.c
+++ chapter_13/main.c
@@ -17,18 +17,7 @@ int main() {
 
   int arr[4] = {-78, 56, 12, 67};
 
-  int *p1 = arr;
-
-  // while (p1 < arr + 4) {
-  //   printd(*p1++);
-  // }
-
-  int x;
-  for (x = 0; x < x + 10; x++)
-    printd(x);
-
-  // while (x < x + 9)
-  //   printd(x);
+  printd(*arr);
 
   /*
   char *input = "aaaabaaaa";
