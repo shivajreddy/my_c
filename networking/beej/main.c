0a1f87a873d52a91c1b751c3d7e0a62d7ec8b372
shiva <shivajreddy@outlook.com>
2025-01-06 16:49:24 -0500

    .

---
+++ networking/beej/main.c
@@ -0,0 +1,18 @@
+#include <fcntl.h> // For open()
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // for read() write() close()
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define BUFFER_SIZE 256
+
+int main() {
+  puts("NETWORKING");
+
+  // getaddrinfo();
+
+  return EXIT_SUCCESS;
+}
