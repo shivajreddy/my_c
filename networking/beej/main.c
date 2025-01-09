2c12efa4a0dcb9b921ddda8d355dbed59565ae8f
smpl <shivajreddy@outlook.com>
2025-01-09 18:39:44 -0500

    .

--- networking/beej/main.c
+++ networking/beej/main.c
@@ -1,6 +1,9 @@
+#include <arpa/inet.h>
 #include <fcntl.h> // For open()
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h> // for read() write() close()
 
 #include <netdb.h>
@@ -12,7 +15,20 @@
 int main() {
   puts("NETWORKING");
 
-  // getaddrinfo();
+  int status;
+  struct addrinfo hints = {.ai_family = AF_UNSPEC,
+                           .ai_socktype = SOCK_STREAM,
+                           .ai_flags = AI_PASSIVE};
+  struct addrinfo *servinfo; // will point to the reslts
+
+  memset(&hints, 0, sizeof hints);
+
+  if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+
+  freeaddrinfo(servinfo);
 
   return EXIT_SUCCESS;
 }
