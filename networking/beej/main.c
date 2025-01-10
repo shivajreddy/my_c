d20da48b7dbde98e4738abf03446fd13f1964e03
shiva <shivajreddy@outlook.com>
2025-01-10 07:55:16 -0500

    .

--- networking/beej/main.c
+++ networking/beej/main.c
@@ -1,8 +1,10 @@
 #include <fcntl.h> // For open()
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // for read() write() close()
 
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -11,6 +13,9 @@
 
 int main() {
   puts("NETWORKING");
+
+  struct sockaddr_in sa;
+  inet_pton(AF_INET, "10.12.110.57", &(sa.sin_addr));
 
   // getaddrinfo();
 
