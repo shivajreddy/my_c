79270e75d55239b42b9a21d81b6e46dcdcd67475
smpl <shivajreddy@outlook.com>
2025-01-13 06:18:23 -0500

    .

--- book/beej/ch19/main.c
+++ book/beej/ch19/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+#define HELLO
+
 int main(void) {
   printf("hello\n");
   printf("hello\n");
+
+#ifdef HELLO
+  printf("HEllo is defined");
+#else
+  printf("HEllo is not-defined");
+#endif
+
   return 0;
 }
