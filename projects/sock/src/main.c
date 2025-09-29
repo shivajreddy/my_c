39f4c70a1e3348d70a1b1935f8aa33199314d797
shiva <sreddy@hey.com>
2025-09-29 16:30:19 -0400

    .

--- projects/sock/src/main.c
+++ projects/sock/src/main.c
@@ -1,7 +1,29 @@
+#include <netinet/in.h>
 #include <stdio.h>
 
+int server(int portno);
+int client(const char* hostname, int portno);
+
 int main(void) {
-    printf("Hello, World!\n");
+    printf("SOCKET SERVER IN C\n");
+
+    int id = server(9188);
+    printf("id: %d\n", id);
+
+    /*
+    int fd = socket(AF_INET6, SOCK_DGRAM | SOCK_NONBLOCK, 0);
+    printf("res = %d\n", fd);
+
+    int flags = fcntl(fd, F_GETFL);
+    if (flags == -1) return 1;
+    if (flags & O_NONBLOCK) {
+        printf("Socket is NON-BLOCKING \n");
+    } else {
+        printf("Socket is BLOCKING\n");
+    }
+
+    close(fd);
+    */
 
     return 0;
 }
---
+++ projects/sock/src/server.c
@@ -0,0 +1,59 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int server(int portno) {
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    char buffer[256];
+    int n;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    struct sockaddr_in serv_addr = { .sin_family = AF_INET6,
+                                     .sin_addr.s_addr = INADDR_ANY,
+                                     .sin_port = htons(portno) };
+    struct sockaddr* serv_addr = malloc(sizeof(struct sockaddr));
+    struct sockaddr_in cli_addr;
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+    if (newsockfd < 0) {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
+    memset(buffer, 0, 256);
+    n = read(newsockfd, buffer, 255);
+    if (n < 0) {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+    printf("Here is the message: %s\n", buffer);
+
+    n = write(newsockfd, "I got your message", 18);
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
